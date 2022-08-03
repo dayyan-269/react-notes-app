8248811b1a530a2a9d10857e5988b0925c26e39c
Dayyan Syehan <syehan269@gmail.com>
2022-08-03 15:24:41 +0700

    change the primary btn style and add new Delete btn

---
+++ src/components/buttons/DeleteButton.jsx
@@ -0,0 +1,20 @@
+function DeleteButton({
+  description,
+  className,
+  type = 'button',
+  onclick,
+  id,
+}) {
+  return (
+    <button
+      id={id}
+      className={`rounded-sm border-0 bg-green-600 px-5 py-2 text-white duration-150 hover:-translate-y-1 hover:drop-shadow-2xl ${className}`}
+      type={type}
+      onClick={onclick}
+    >
+      {description}
+    </button>
+  )
+}
+
+export default DeleteButton
--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,8 +1,18 @@
-function PrimaryButton({ description, className, type = 'button' }) {
+function PrimaryButton({
+  id,
+  description,
+  className,
+  type = 'button',
+  onClick,
+  disabled,
+}) {
   return (
     <button
-      className={`rounded-sm border-0 bg-green-600 px-5 py-2 text-white duration-150 hover:-translate-y-1 hover:drop-shadow-2xl ${className}`}
+      id={id}
+      className={`rounded-sm bg-green-600 px-5 py-2 text-white duration-150 hover:-translate-y-1 hover:drop-shadow-2xl ${className}`}
       type={type}
+      onClick={onClick}
+      disabled={disabled}
     >
       {description}
     </button>
