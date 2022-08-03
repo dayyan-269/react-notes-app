24fa9bea47abb98c5236c4daac47050294a51af5
Dayyan Syehan <syehan269@gmail.com>
2022-08-03 20:18:39 +0700

    add onchange prop on regular checkbox

--- src/components/inputs/RegularCheckbox.jsx
+++ src/components/inputs/RegularCheckbox.jsx
@@ -1,8 +1,8 @@
-function RegularCheckbox({ description, onChange }) {
+function RegularCheckbox({ description, checked, onChange }) {
   return (
     <div className="flex flex-row gap-2">
       <label htmlFor="check">{description}</label>
-      <input type="checkbox" id="check" onChange={onChange} />
+      <input type="checkbox" id="check" checked={checked} onChange={onChange} />
     </div>
   )
 }
