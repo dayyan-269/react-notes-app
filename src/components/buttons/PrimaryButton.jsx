8bb41755645fd2082e55acbb92ae1c9e1df92e4a
Dayyan Syehan <syehan269@gmail.com>
2022-08-03 20:18:21 +0700

    remove the background of primary button

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -4,15 +4,17 @@ function PrimaryButton({
   className,
   type = 'button',
   onClick,
+  onSubmit,
   disabled,
 }) {
   return (
     <button
       id={id}
-      className={`rounded-sm bg-green-600 px-5 py-2 text-white duration-150 hover:-translate-y-1 hover:drop-shadow-2xl ${className}`}
+      className={`rounded-sm px-5 py-2 duration-150 hover:-translate-y-1 hover:drop-shadow-2xl ${className}`}
       type={type}
       onClick={onClick}
       disabled={disabled}
+      onSubmit={onSubmit}
     >
       {description}
     </button>
